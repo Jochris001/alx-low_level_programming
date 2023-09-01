e0132089233838615e877432a917e61658010f89
jochris001 <jochris700@gmail.com>
2023-09-01 05:35:12 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *to get_endianness - one needs to checks if a machine is little or big endian
+ * Return: use 0 for big, use 1 for little
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *) &i;
+
+	return (*c);
+}
